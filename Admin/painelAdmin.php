8c4f36bc10572261425a5c8fde0a93bf67496a25
Guilherme Davi Franco <guilhermedavifranco@gmail.com>
2022-12-21 10:34:47 -0300

    Start

---
+++ Admin/painelAdmin.php
@@ -0,0 +1,45 @@
+<?php
+ include "head.php";
+?>
+
+<body class="bg-gray-800 font-sans leading-normal tracking-normal mt-12">
+    <?php
+    include "header.php";
+    include "nav.php";
+
+    switch($_GET['r']){
+
+        case 'menu':
+                include "views/menu.php";
+                break;
+        case 'today':
+                include "views/today.php";
+                break;
+        case 'concluidos':
+                include "views/concluidos.php";
+                break;
+        case 'cancelados':
+                include "views/cancelados.php";
+                break;
+        case 'lojaCopy':
+                include "views/lojaCopy.php";
+                break;
+        case 'estabelecimento':
+                include "views/estabelecimento.php";
+                break;
+        default:
+                include "views/menu.php";
+                break;
+            
+    }
+    
+
+
+    ?>
+    
+    
+ <?php include "js.php"; ?>
+ 
+
+</body>
+</html>
